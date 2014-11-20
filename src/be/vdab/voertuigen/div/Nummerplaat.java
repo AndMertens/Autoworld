1c29d00847ab99b6631c282d9f30936b51cfaaa6
Andy.mertens <Andy.mertens@ONTCUR-0E18035F.vdabheverlee.intra>
2014-11-20 16:00:20 +0100

    Kopie gemaakt van master autoworld

--- src/be/vdab/voertuigen/div/Nummerplaat.java
+++ src/be/vdab/voertuigen/div/Nummerplaat.java
@@ -5,15 +5,42 @@
 
 package be.vdab.voertuigen.div;
 
+import java.io.Serializable;
+
 /**
  *
  * @author frank.roelants
  */
-public class Nummerplaat {
+public class Nummerplaat implements Serializable,Comparable<Nummerplaat> {
     private String plaat;
 
-    public Nummerplaat(String plaat) {
+    protected Nummerplaat(String plaat) {
         this.plaat = plaat;
+    }
+
+    @Override
+    public int hashCode() {
+        return plaat.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return (obj instanceof Nummerplaat) &&
+               ((Nummerplaat)obj).plaat.equals(this.plaat);
+    }
+
+    @Override
+    public String toString() {
+        return plaat.toString();
+    }
+
+    public String getPlaat() {
+        return plaat;
+    }
+
+    @Override
+    public int compareTo(Nummerplaat o) {
+        return plaat.compareTo(o.plaat);
     }
     
 }
